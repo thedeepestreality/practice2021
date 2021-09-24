6fa21516f869ea83894b8e4274e4bb61bea84a71
thedeepestreality <zetoster@gmail.com>
2021-09-24 18:44:58 +0300

    added functions examples

---
+++ functions.bat
@@ -0,0 +1,32 @@
+@echo off
+REM no args passed
+call :global_return
+echo %result%
+
+REM one arg used for return val also
+call :named_return retval
+echo %retval%
+
+REM passing all the console arguments
+call :iterateargs %*
+exit /b
+
+:global_return
+echo hello
+set result=global_ok
+exit /b
+
+:named_return
+echo named_return passed arg value: %1
+REM set the return value
+set %1=named_ok
+exit /b
+
+:iterateargs
+echo started iterate_args func
+:iterateargs_cycle
+if "%1"=="" exit/b
+echo arg %1
+REM shifting args by 1
+shift
+goto iterateargs_cycle
