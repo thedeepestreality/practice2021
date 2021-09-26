4c82f22d7587279828d1072eaf960b64887420df
thedeepestreality <zetoster@gmail.com>
2021-09-27 02:22:43 +0300

    added examples on loops and delayed expansion

---
+++ delayed_expansion.bat
@@ -0,0 +1,41 @@
+@echo off
+REM inner IF will not work
+REM because it is in fact on the same line
+REM with the outer IF and VAR value read only once
+set VAR=old_val
+if %VAR% == old_val (
+    echo old_val worked!
+    set VAR=new_val
+    if %VAR% == new_val echo new_val worked!
+)
+
+REM this IF will work, because this is new line
+if %VAR% == new_val (
+    echo new_val on the new line worked!
+)
+
+REM here LIST is supposed to grow
+REM but again %LIST% in the loop body will be empty
+set LIST=
+for %%i in (1,2,3) do (
+    set LIST=%LIST% %%i
+)
+echo list without delayed expansion: (%LIST%)
+
+REM echo newline
+echo/
+
+REM and now we enable delayed expansion...
+setlocal enabledelayedexpansion
+set VAR=old_val
+if %VAR% == old_val (
+	echo old_val worked^!
+    set VAR=new_val
+    if !VAR! == new_val echo new_val with delayed expansion worked^!
+)
+
+set LIST=
+for %%i in (1,2,3) do (
+    set LIST=!LIST! %%i
+)
+echo list with delayed expansion: (%LIST%)
---
+++ loops.bat
@@ -0,0 +1,53 @@
+@echo off
+
+REM iterate through the list
+echo SIMPLE FOR LOOP:
+for %%i in (1, abc, 1.2) do (
+    echo %%i
+)
+
+REM echo newline
+echo/
+
+REM FOR tries to interpret list values as filenames
+REM the code below should print all the .bat-file names
+echo/ & echo BAT FILES IN THE CURRENT FOLDER:
+for %%i in (*.bat) do (
+    echo %%i
+)
+REM /R flag allow to search files in the all subfolders recursively
+echo/ & echo BAT FILES IN THE CURRENT FOLDER AND ALL SUBFOLDERS:
+for /R %%i in (*.bat) do (
+    echo %%i
+)
+
+REM /D will interpret list as FOLDER names
+echo/ & echo FOLDERS:
+for /D %%i in (*) do (
+    echo %%i
+)
+
+REM range-base for-loop with /L flag
+REM list syntax: (start, step, end)
+echo/ & echo RANGE-BASED FOR:
+for /L %%i in (1,2,10) do (
+    echo %%i
+)
+
+REM loop for file contents
+echo/ & echo FOR-File:
+for /F "tokens=1,2" %%i in (check_arg.bat) do (
+    echo %%i %%j
+)
+
+REM loop for string contents
+echo/ & echo FOR-String:
+for /F "tokens=1,2" %%i in ("first second") do (
+    echo %%i %%j
+)
+
+REM loop for command result
+echo/ & echo FOR-Command:
+for /F "tokens=1,2" %%i in ('echo first second') do (
+    echo %%i %%j
+)
