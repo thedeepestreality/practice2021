1615dcdf482993527a9c18655034c66b5bd7a57c
thedeepestreality <zetoster@gmail.com>
2021-12-06 16:41:06 +0300

    added printImgInfo function body

--- examples/BmpImg/BmpImg/ImgFunctions.cpp
+++ examples/BmpImg/BmpImg/ImgFunctions.cpp
@@ -1,4 +1,7 @@
 #include "ImgFunctions.h"
+#include <fstream>
+#include <stdexcept>
+#include <iostream>
 
 RgbImg readRgbImg(const char* filename) 
 {
@@ -8,4 +11,27 @@ RgbImg readRgbImg(const char* filename)
 
 void writeRgbImg(char const* filename, RgbImg const& img) {}
 
-void printImgInfo(const char* filename) {}
+void printImgInfo(const char* filename)
+{
+    std::ifstream bmp_in(filename, std::ios::binary);
+    if (!bmp_in.is_open())
+        throw std::runtime_error("Failed to open bmp file");
+
+    BITMAPFILEHEADER bmp_file_info;
+    bmp_in.read((char*)&bmp_file_info, sizeof(BITMAPFILEHEADER));
+
+    std::cout << "File " << filename << "\n";
+    std::cout << "Type: " << ((char*)&bmp_file_info.bfType)[0];
+    std::cout << " " << ((char*)&bmp_file_info.bfType)[1] << "\n";
+    std::cout << "File size: " << bmp_file_info.bfSize << "\n";
+    std::cout << "Img offset: " << bmp_file_info.bfOffBits << "\n";
+
+    BITMAPINFOHEADER bmp_img_info;
+    bmp_in.read((char*)&bmp_img_info, sizeof(BITMAPINFOHEADER));
+    
+    std::cout << "Width: " << bmp_img_info.biWidth << "\n";
+    std::cout << "Height: " << bmp_img_info.biHeight << "\n";
+    std::cout << "Depth: " << bmp_img_info.biBitCount << "\n";
+
+    bmp_in.close();
+}
--- examples/BmpImg/BmpImg/Source.cpp
+++ examples/BmpImg/BmpImg/Source.cpp
@@ -4,6 +4,6 @@
 
 int main()
 {
-
+	printImgInfo("masterpiece.bmp");
 	return 0;
 }
