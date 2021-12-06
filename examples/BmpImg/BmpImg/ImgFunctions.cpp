53cc0c95e7123099814b4db2ef2b6e48a5323843
thedeepestreality <zetoster@gmail.com>
2021-12-06 17:08:44 +0300

    rgb img functions coded

--- examples/BmpImg/BmpImg/ImgFunctions.cpp
+++ examples/BmpImg/BmpImg/ImgFunctions.cpp
@@ -3,13 +3,80 @@
 #include <stdexcept>
 #include <iostream>
 
+int getOffset(int width)
+{
+    int offset = 0;
+    if (width % 4 != 0)
+        offset = 4 - (3 * width) % 4;
+    return offset;
+}
+
 RgbImg readRgbImg(const char* filename) 
 {
-    RgbImg img = { nullptr, 0, 0 };
+    std::ifstream bmp_in(filename, std::ios::binary);
+    if (!bmp_in.is_open())
+        throw std::runtime_error("Failed to open input bmp file");
+
+    BITMAPFILEHEADER bmp_file_info;
+    BITMAPINFOHEADER bmp_img_info;
+    bmp_in.read((char*)&bmp_file_info, sizeof(BITMAPFILEHEADER));
+    bmp_in.read((char*)&bmp_img_info, sizeof(BITMAPINFOHEADER));
+
+    int const offset = getOffset(bmp_img_info.biWidth);
+    RgbImg img;
+    img.width = bmp_img_info.biWidth;
+    img.height = bmp_img_info.biHeight;
+    img.pixels = new RGB * [img.height];
+    for (size_t row = 0; row < img.height; ++row)
+    {
+        img.pixels[row] = new RGB[img.width];
+        for (size_t col = 0; col < img.width; ++col)
+            bmp_in.read((char*)&img.pixels[row][col], 3);
+        bmp_in.seekg(offset, std::ios::cur);
+    }
+    bmp_in.close();
     return img;
 }
 
-void writeRgbImg(char const* filename, RgbImg const& img) {}
+void writeRgbImg(char const* filename, RgbImg const& img) 
+{
+    std::ofstream bmp_out(filename, std::ios::binary);
+    if (!bmp_out.is_open())
+        throw std::runtime_error("Failed to open output bmp file");
+    int offset = getOffset(img.width);
+    BITMAPFILEHEADER bmp_file_info;
+    char bfType[] = { 'B', 'M' };
+    bmp_file_info.bfType = *((WORD*)bfType);
+    bmp_file_info.bfOffBits = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
+    bmp_file_info.bfSize = bmp_file_info.bfOffBits + (img.height * (img.width + offset)) * 3;
+    bmp_file_info.bfReserved1 = bmp_file_info.bfReserved2 = 0;
+
+    BITMAPINFOHEADER bmp_img_info;
+    bmp_img_info.biClrImportant = bmp_img_info.biClrUsed = bmp_img_info.biCompression = 0;
+    bmp_img_info.biPlanes = bmp_img_info.biXPelsPerMeter = bmp_img_info.biYPelsPerMeter = 1;
+    bmp_img_info.biSize = sizeof(BITMAPINFOHEADER);
+    bmp_img_info.biSizeImage = bmp_file_info.bfSize - bmp_file_info.bfOffBits;
+    bmp_img_info.biWidth = img.width;
+    bmp_img_info.biHeight = img.height;
+    bmp_img_info.biBitCount = 24;
+
+    bmp_out.write((char*)&bmp_file_info, sizeof(BITMAPFILEHEADER));
+    bmp_out.write((char*)&bmp_img_info, sizeof(BITMAPINFOHEADER));
+
+    BYTE* offset_array = new BYTE [offset];
+    for (size_t i = 0; i < offset; ++i)
+        offset_array[i] = 0;
+
+    for (size_t row = 0; row < img.height; ++row)
+    {
+        for (size_t col = 0; col < img.width; ++col)
+            bmp_out.write((char*)&img.pixels[row][col], 3);
+        bmp_out.write((char*)offset_array, offset);
+    }
+    
+    delete[] offset_array;
+    bmp_out.close();
+}
 
 void printImgInfo(const char* filename)
 {
--- examples/BmpImg/BmpImg/Source.cpp
+++ examples/BmpImg/BmpImg/Source.cpp
@@ -5,5 +5,19 @@
 int main()
 {
 	printImgInfo("masterpiece.bmp");
+	RgbImg img = readRgbImg("masterpiece.bmp");
+	for (size_t row=0; row < 50;++row)
+		for (size_t col = 0; col < 50; ++col)
+		{
+			img.pixels[row][col].Red = 255;
+			img.pixels[row][col].Blue = 255;
+			img.pixels[row][col].Green = 255;
+		}
+
+	writeRgbImg("ugly_copy.bmp", img);
+
+	for (size_t row = 0; row < img.height; ++row)
+		delete[] img.pixels[row];
+	delete[] img.pixels;
 	return 0;
 }
